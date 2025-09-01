92da7f2129fc95eae0694485d4296ab178e04f7a
Priyabrata Das <daspriyabrat10@gmail.com>
2025-09-01 23:46:21 +0530

    fixed Credentials Provider import

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,5 @@
 import { NextAuthOptions } from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import  CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import { dbConnect } from "@/lib/dbConnect";
 import UserModel from "@/model/User";
@@ -13,7 +13,39 @@ export const authOptions: NextAuthOptions = {
                 email: { label: "Email", type: "text" },
                 password: { label: "Password", type: "password" }
             },
-            async authorize()
-        })
+            async authorize(credentials : any) : Promise<any>{
+                await dbConnect()
+                try {
+                    // while finding user from db 
+                    // its written as -> credentials.identifier
+                  const user =  await UserModel.findOne({
+                        $or : [
+                            {email : credentials.identifier},
+                            {username : credentials.identifier}
+                        ]
+                    })
+
+                    if(!user){
+                        throw new Error("No user found with this email")
+                    }
+                    
+                    if(!user.isVerified){
+                        throw new Error("Please verify your account first before login")
+                    }
+
+                    //  while comparing with bcrypt
+                    //  its written as -> credentials.password
+                    // not as -> credentials.identifier.password
+                   const isPassCorrect = await bcrypt.compare(credentials.password, user.password) 
+
+                   if(isPassCorrect) return user
+                   else throw new Error('Incorrect user')
+
+                } catch (error: any) {
+                    throw new Error(error)
+                }
+
+            }
+        }),
     ]
 }
